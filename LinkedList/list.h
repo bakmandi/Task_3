9fd0b4e2c82418152f857c962e5d4c25ed0ed31c
bakmandi <explodingc4@gmail.com>
2015-09-19 22:03:29 +0700

    Update list.h

--- LinkedList/list.h
+++ LinkedList/list.h
@@ -1,6 +1,33 @@
 #ifndef LIST_H_INCLUDED
 #define LIST_H_INCLUDED
 
+typedef struct kelas {
+    char id[10];
+    char tabungan_asuransi[25];
+    char jenis_asuransi;
+};
 
+typedef struct elmlist *address;
+
+typedef struct elmlist{
+    kelas data;
+    address next;
+};
+
+typedef struct list {
+    address first;
+};
+
+void createlist(list *l);
+void dealokasi(address p);
+address alokasi(data x);
+
+void insertfirst(list *l, address p);
+void insertlast(list *l, address p);
+void insertafter(address q, address p);
+void deletefirst(list *l, address p);
+void deletelast(list *l, address p);
+void deleteafter(address *p);
+void printinfo(list l);
 
 #endif // LIST_H_INCLUDED
